1c74a2598839c23fc5cad7de14796a9ee4a7e950
LeoCyb <68962803+LeoCyb@users.noreply.github.com>
2020-07-29 18:15:30 +0200

    Add files via upload

---
+++ Calcul.py
@@ -0,0 +1,24 @@
+from matplotlib import pyplot as plt
+from math import *
+
+
+def nb_kg_co2(nb_annee, cout_installation, kwh_par_an, rendement, kgco2_par_kwh):
+    return cout_installation + nb_annee * kwh_par_an / rendement * kgco2_par_kwh
+
+
+plt.style.use("seaborn")
+x_pompe_a_chaleur = range(0, 11)
+y_pompe_a_chaleur = [nb_kg_co2(n, 1563, 4000, 2, 0.08) for n in x_pompe_a_chaleur]
+
+x_chaudiere_charbon = range(0, 11)
+y_chaudiere_charbon = [nb_kg_co2(n, 0, 4000, 0.9, 0.300) for n in x_chaudiere_charbon]
+
+plt.plot(x_pompe_a_chaleur, y_pompe_a_chaleur, label="Pompe à chaleur")
+plt.plot(x_chaudiere_charbon, y_chaudiere_charbon, label="Chaudière à charbon")
+
+plt.xlabel("Nombre d'années")
+plt.ylabel("kg de CO2 émis")
+plt.title(label="kg de CO2 émis cumulé selon l'appareil en France pour 4000KWh d'énergie thermique fourni par an", fontsize=11)
+plt.legend()
+plt.tight_layout()
+plt.show()
